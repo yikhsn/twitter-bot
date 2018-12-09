acee26ddcf53937ea21bc66e1aa4d8f1e11dd0bf
Yaumil Ikhsan <yaumilikhsan98@gmail.com>
2018-12-09 15:42:32 +0700

    bot tweeting added

--- bot.js
+++ bot.js
@@ -1,15 +1,40 @@
 const config = require('./config');
 const Twit = require('twit');
 
-const Twitter = new Twit(config);
+const T = new Twit(config);
 
-Twitter.post(
-    'statuses/update',
-    { status: 'This is an automated test!' },
-    (err, data, response) => {
-      console.log(err, data, response);
+T.get('account/verify_credentials', {
+  include_entities: false,
+  skip_status: true,
+  include_email: false
+}, onAuthenticated);
+
+function onAuthenticated(err, res) {
+  if (err) {
+      throw err
+  }
+
+  console.log('Authentication successful. Running bot...\r\n')
+}
+
+const tweet = (status) => {
+  T.post( 'statuses/update', { 
+    status: status
+  }, (err, data, response) => {
+    if (response) {
+      console.log(`Tweeted`);
     }
-  )
+    if (err){
+      console.log(err, data);
+    }
+  });
+};
+
+setInterval(() => {
   
-// Retweet a given tweet
-Twitter.post('statuses/retweet/:id', { id: '697162548957700096' })
+  const min = 1;
+  const max = 6223;
+  const ran = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  tweet(ran);
+}, 36000);
